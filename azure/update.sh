48f0863c29f204933f2a1e23baf8c386cb5e5ff7
Maciej Laskowski <skejven@gmail.com>
2020-02-24 10:00:42 +0100

    Fixed #25 - cross repository PR ci fix

--- azure/update.sh
+++ azure/update.sh
@@ -16,20 +16,16 @@ rm -rf knotx-repos
 repos=()
 IFS=$'\n' read -d '' -r -a repos < ../repositories.cfg
 
-org=''
-project=''
 TIMESTAMP=`date "+%d%b%Y%H%M%S"`
 BRANCH=feature/azure-pipeline-upgrade-$TIMESTAMP
+DEFAULT_ORG="Knotx"
 
-for repo in "${repos[@]}"
+for project in "${repos[@]}"
 do
-  org=`echo "$repo" | cut -d';' -f1`
-  project=`echo "$repo" | cut -d';' -f2`
-
-  operation="Updating Azure Pipelines configuration: $org/$project to knotx-repos/$project"
+  operation="Updating Azure Pipelines configuration: $DEFAULT_ORG/$project to knotx-repos/$project"
   echo "$operation"
 
-  git clone --depth 1 "git@github.com:$org/$project.git" "knotx-repos/$project"
+  git clone --depth 1 "git@github.com:$DEFAULT_ORG/$project.git" "knotx-repos/$project"
   git --git-dir=knotx-repos/${project}/.git --work-tree=knotx-repos/${project} checkout -b $BRANCH
 
   cp "./azure-pipelines.yml" "knotx-repos/$project"
--- development/pull-all.sh
+++ development/pull-all.sh
@@ -1,8 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # eval ssh
 # eval `ssh-agent`
 # ssh-add ~/.ssh/id_rsa
+
+DEFAULT_ORG="Knotx"
 
 #########################
 # The command line help #
@@ -15,6 +17,7 @@ help() {
     echo "   -c                         create GIT branch if not exists and sets the tracking"
     echo "   -f                         reset all changes if repository is cloned"
     echo "   -m                         merge / rebase with branch '-m original/master'"
+    echo "   -o                         organization '-o Knotx' - this option works only when repository was not cloned already"
     echo "   -a                         clone with HTTPS instead of clone with SSH (defult)"
     echo
     echo "Examples:"
@@ -22,6 +25,7 @@ help() {
     echo "   sh pull-all.sh -r projects/knotx -b feature/some-branch -c                   clones (if not exists) all repositories to 'projects/knotx' folder and switches to 'feature/some-branch' (if not exists create the branch)"
     echo "   sh pull-all.sh -r projects/knotx -b feature/some-branch -f                   clones (if not exists) all repositories to 'projects/knotx' folder and switches to 'feature/some-branch' (discard all modifications)"
     echo "   sh pull-all.sh -r projects/knotx -b feature/some-branch -m origin/master     clones (if not exists) all repositories to 'projects/knotx' folder, switches to 'feature/some-branch', rebases with 'origin/master', pushes changes"
+    echo "   sh pull-all.sh -r projects/knotx -b master -o Custom                         clones (if not exists) all repositories from https://github.com/Custom (instead of https://github.com/Knotx, which is fallback when Custom does not contain the repo) organization to 'projects/knotx' folder and switches to master branch"
     exit 1
 }
 
@@ -29,53 +33,61 @@ help() {
 # GIT checkout with branch #
 ############################
 checkout() {
-  # $1 organization
-  # $2 repository name
+  GITHUB_ORG=$1
+  GITHUB_REPO=$2
+
   echo "***************************************"
-  echo "* Checking out git@github.com:$1/$2.git"
+  echo "* Checking out git@github.com:$GITHUB_ORG/$GITHUB_REPO.git"
   echo "***************************************"
 
-  if [[ -d $2 ]]; then
+  if [[ -d $GITHUB_REPO ]]; then
     if [[ $FORCE ]]; then
-      git --git-dir=$2/.git --work-tree=$2 reset HEAD --hard
+      git --git-dir=$GITHUB_REPO/.git --work-tree=$GITHUB_REPO reset HEAD --hard
     fi
-    git --git-dir=$2/.git --work-tree=$2 fetch
+    git --git-dir=$GITHUB_REPO/.git --work-tree=$GITHUB_REPO fetch
   else
+
+    status="$(curl -s --head -w %{http_code} "https://github.com/$GITHUB_ORG/$GITHUB_REPO" -o /dev/null)"
+    if [ $status != "200" ]; then
+      echo "Can't access https://github.com/$GITHUB_ORG/$GITHUB_REPO, will clone https://github.com/$DEFAULT_ORG/$GITHUB_REPO instead"
+      GITHUB_ORG=$DEFAULT_ORG
+    fi
+
     if [[ $HTTPS ]]; then
-      git clone "https://github.com/$1/$2.git"
+      git clone "https://github.com/$GITHUB_ORG/$GITHUB_REPO.git"
     else
-      git clone "git@github.com:$1/$2.git"
+      git clone "git@github.com:$GITHUB_ORG/$GITHUB_REPO.git"
     fi
   fi
 
-  git --git-dir=$2/.git --work-tree=$2 checkout master
-  git --git-dir=$2/.git --work-tree=$2 pull
+  git --git-dir=$GITHUB_REPO/.git --work-tree=$GITHUB_REPO checkout master
+  git --git-dir=$GITHUB_REPO/.git --work-tree=$GITHUB_REPO pull
 
   # checks if branch exists, otherwise use master branch
-  if [[ `git --git-dir=$2/.git --work-tree=$2 branch --list --all | grep $BRANCH` ]]; then
-    git --git-dir=$2/.git --work-tree=$2 checkout $BRANCH
+  if [[ `git --git-dir=$GITHUB_REPO/.git --work-tree=$GITHUB_REPO branch --list --all | grep $BRANCH` ]]; then
+    git --git-dir=$GITHUB_REPO/.git --work-tree=$GITHUB_REPO checkout $BRANCH
     if [[ $MERGE ]]; then
-      git --git-dir=$2/.git --work-tree=$2 rebase $MERGE
-      git --git-dir=$2/.git --work-tree=$2 push
+      git --git-dir=$GITHUB_REPO/.git --work-tree=$GITHUB_REPO rebase $MERGE
+      git --git-dir=$GITHUB_REPO/.git --work-tree=$GITHUB_REPO push
     fi
   else
     if [[ $CREATE ]]; then
-      git --git-dir=$2/.git --work-tree=$2 checkout -b $BRANCH
+      git --git-dir=$GITHUB_REPO/.git --work-tree=$GITHUB_REPO checkout -b $BRANCH
       if [[ $MERGE ]]; then
-        git --git-dir=$2/.git --work-tree=$2 rebase $MERGE
-        git --git-dir=$2/.git --work-tree=$2 push
+        git --git-dir=$GITHUB_REPO/.git --work-tree=$GITHUB_REPO rebase $MERGE
+        git --git-dir=$GITHUB_REPO/.git --work-tree=$GITHUB_REPO push
       fi
-      git --git-dir=$2/.git --work-tree=$2 branch --set-upstream-to=origin/$BRANCH $BRANCH
+      git --git-dir=$GITHUB_REPO/.git --work-tree=$GITHUB_REPO branch --set-upstream-to=origin/$BRANCH $BRANCH
     fi
   fi
-  git --git-dir=$2/.git --work-tree=$2 pull
+  git --git-dir=$GITHUB_REPO/.git --work-tree=$GITHUB_REPO pull
 }
 
 #########################
 #          Main         #
 #########################
 
-while getopts hr:b:fcm:a option
+while getopts hr:b:fcm:o:a option
 do
   case "${option}"
     in
@@ -85,11 +97,15 @@ do
     f) FORCE=true;;
     c) CREATE=true;;
     m) MERGE=${OPTARG};;
+    o) ORGANIZATION=${OPTARG};;
     a) HTTPS=true;;
   esac
 done
 
+ORG="${ORGANIZATION:-$DEFAULT_ORG}"
+
 echo "Script root catalogue [$ROOT]"
+echo "GitHub organization [$ORG ($ORGANIZATION)]"
 echo "GIT branch name [$BRANCH]"
 
 if [[ $FORCE ]]; then
@@ -112,24 +128,30 @@ IFS=$'\n' read -d '' -r -a repos < ../repositories.cfg
 
 cd $ROOT
 
-org=''
-project=''
 for repo in "${repos[@]}"
 do
-  org=`echo "$repo" | cut -d';' -f1`
-  project=`echo "$repo" | cut -d';' -f2`
-  checkout $org $project
+  checkout $ORG $repo
 done
 
 echo "***************************************"
 echo "SUMMARY"
 echo "***************************************"
 
+echo `pwd`
 for repo in "${repos[@]}"
 do
-  org=`echo "$repo" | cut -d';' -f1`
-  project=`echo "$repo" | cut -d';' -f2`
-  printf "%-30s %s\n" "$org/$project" `git --git-dir=$project/.git --work-tree=$project branch | grep \* | cut -d ' ' -f2`
+  repo_url=$(git --git-dir=$repo/.git --work-tree=$repo config --get remote.origin.url 2>&1)
+  if [[ $repo_url == http* ]]; then
+    origin=`echo $repo_url | cut -d'.' -f2 | cut -c 5-`
+  else
+    origin=`echo $repo_url | cut -d':' -f2 | cut -d'.' -f1`
+  fi
+
+  repo_org=$(cut -d'/' -f1 <<<$origin)
+  repo_name=$(cut -d'/' -f2 <<<$origin)
+  branch=`git --git-dir=$repo/.git --work-tree=$repo branch | grep \* | cut -d ' ' -f2`
+
+  printf "%10s %30s %20s %s\n" $repo_org $repo_name $branch
 done
 
 # allows to import all modules in IDEA as one project
